abddc48188771389e8f2cd1d4df39a443af244f2
Bruna Nunes <bruna@websix.com.br>
2022-03-31 23:34:08 -0300

    Rota 403 e 404

--- routes/routes.php
+++ routes/routes.php
@@ -7,3 +7,16 @@ SimpleRouter::get('/', 'HomeController@index');
 SimpleRouter::get('/user', 'UserController@index');
 SimpleRouter::get('/user/store', 'UserController@store');
 
+SimpleRouter::error(function(Request $request, \Exception $exception) {
+
+    switch($exception->getCode()) {
+        // Page not found
+        case 404:
+            response()->redirect('/not-found');
+        // Forbidden
+        case 403:
+            response()->redirect('/forbidden');
+    }
+    
+});
+
