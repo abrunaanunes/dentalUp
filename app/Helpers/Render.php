5add778e9da44d2d5d8ca400ec90ecb59ae8146c
Bruna Nunes <bruna@websix.com.br>
2022-04-04 20:31:29 -0300

    Helper para renderizar html WIP

---
+++ app/Helpers/Render.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\Helpers;
+
+trait Render {
+    public function RenderHtml(string $view, array $data)
+    {
+        // extract($data);
+        // ob_start();
+        // require $_SERVER['DOCUMENT_ROOT'] . '/' .'views/' . $view . '.html';
+        // $html = ob_get_clean();
+
+        // return $html;
+        echo 'ok';
+    }
+}
