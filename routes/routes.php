139776193532f051d36bd9df11c1264949f63bbe
Bruna Nunes <bruna@websix.com.br>
2022-04-04 20:30:50 -0300

    Refatorando rotas e views

--- routes/routes.php
+++ routes/routes.php
@@ -2,21 +2,44 @@
 
 use Pecee\SimpleRouter\SimpleRouter;
 
-SimpleRouter::get('/', 'HomeController@index');
-
-SimpleRouter::get('/user', 'UserController@index');
-SimpleRouter::get('/user/store', 'UserController@store');
-
-SimpleRouter::error(function(Request $request, \Exception $exception) {
-
-    switch($exception->getCode()) {
-        // Page not found
-        case 404:
-            response()->redirect('/not-found');
-        // Forbidden
-        case 403:
-            response()->redirect('/forbidden');
-    }
-    
+SimpleRouter::group([
+    'namespace' => 'Front'
+],function () {
+    SimpleRouter::get('/', 'FrontController@index');
+    SimpleRouter::get('/login', 'FrontController@login');
+    SimpleRouter::get('/register', 'FrontController@register');
 });
+
+SimpleRouter::group([
+        'prefix' => 'user',
+        'namespace' => 'Admin'
+    ],function () {
+        SimpleRouter::get('/', 'UserController@index');
+        SimpleRouter::get('/create', 'UserController@create');
+        SimpleRouter::get('/create', 'UserController@create');
+        SimpleRouter::get('/store', 'UserController@store');
+        SimpleRouter::get('/edit', 'UserController@edit');
+        SimpleRouter::get('/update', 'UserController@update');
+        SimpleRouter::get('/destroy', 'UserController@destroy');
+});
+
+SimpleRouter::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+],function () {
+    SimpleRouter::get('/', 'DashboardController@index');
+});
+
+// SimpleRouter::error(function(Request $request, \Exception $exception) {
+
+//     switch($exception->getCode()) {
+//         // Page not found
+//         case 404:
+//             response()->redirect('/not-found');
+//         // Forbidden
+//         case 403:
+//             response()->redirect('/forbidden');
+//     }
+    
+// });
 
