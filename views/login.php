f235cc50aabec62cb633b85782b1c4dd4a9e09d6
Maria Eduarda <mariaesfreitas@gmail.com>
2022-04-11 13:13:53 -0300

    separando pagina de login e register

--- views/login.php
+++ views/login.php
@@ -11,45 +11,14 @@
 </head>
 <body>
     <div class="container">
-        <div class="content first-content">
-            <div class="first-column">
-                <h2 class="title title-primary">Bem vindo!</h2>
-                <p class="description description-primary">Para se conectar, logue com suas informações.</p>
-                <button id="signin" class="btn btn-primary">Entrar</button>
-            </div>    
-            <div class="second-column">
-                <h2 class="title title-second">Criar conta</h2>
-                <p class="description description-second">usando seu e-mail para se registrar:</p>
-                <form class="form">
-                    <label class="label-input" for="">
-                        <i class="far fa-user icon-modify"></i>
-                        <input type="text" placeholder="Nome" name="name">
-                    </label>
-                    
-                    <label class="label-input" for="">
-                        <i class="far fa-envelope icon-modify"></i>
-                        <input type="email" placeholder="E-mail" name="email">
-                    </label>
-                    
-                    <label class="label-input" for="">
-                        <i class="fas fa-lock icon-modify"></i>
-                        <input type="password" placeholder="Senha" name="password">
-                    </label>
-
-                    <label class="label-input" for="">
-                        <i class="fas fa-lock icon-modify"></i>
-                        <input type="password" placeholder="Confirme sua senha" name="password">
-                    </label>
-                    <button class="btn btn-second">Registrar-se</button>        
-                </form>
-            </div>
-        </div>
         <div class="content second-content">
             <div class="first-column">
                 <h2 class="title title-primary">Bem vindo!</h2>
-                <p class="description description-primary">Informe seus dados pessoais</p>
-                <p class="description description-primary">e junte-se a nós.</p>
-                <button id="signup" class="btn btn-primary">Registrar-se</button>
+                <p class="description description-primary">Se você ainda não tem uma conta,</p>
+                <p class="description description-primary">clique abaixo para se registrar.</p>
+                <a href="/register">
+                    <button id="signup" class="btn btn-primary">Registrar-se</button>
+                </a>
             </div>
             <div class="second-column">
                 <h2 class="title title-second">Entre na sua conta</h2>
@@ -72,19 +41,5 @@
             </div>
         </div>
     </div>
-    <script>
-      var btnSignin = document.querySelector("#signin");
-      var btnSignup = document.querySelector("#signup");
-
-      var body = document.querySelector("body");
-
-      btnSignin.addEventListener("click", function () {
-        body.className = "sign-in-js"; 
-      });
-
-      btnSignup.addEventListener("click", function () {
-          body.className = "sign-up-js";
-      })
-    </script>
 </body>
 </html>
